2090e1a79681085e4a577010a58eca3b5245a5e3
H-Nika <88vr07@gmail.com>
2024-05-17 16:40:03 +0300

    after err2

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchContacts, addContact, deleteContact } from "./operations";
+import { logOut } from "../auth/operations";
 
 export const INITIAL_STATE = {
   contacts: {
